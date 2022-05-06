76c4c7315b6aea167fd253045277cf770cdd1049
Prakhar Nagpal <pn320@ic.ac.uk>
2022-05-07 00:03:14 +0100

    can list all members and intents set to all (may change later)

--- bot.py
+++ bot.py
@@ -1,16 +1,28 @@
 # bot.py
 import os
 
-import discord
+from discord import Client, Guild, Intents, utils
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = Intents.all()
+client = Client(intents=intents)
 
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_ready() -> None:
+    guild: Guild | None = utils.find(lambda g: g.name == GUILD, client.guilds)
+    assert guild
+
+    print(
+        f'{client.user} is connected to the following server:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
 
 client.run(TOKEN)
